f34021c11e337e8972d2e801cb141608dad653f7
Leegongseok <seok20108014@gmail.com>
2024-09-03 16:36:01 +0900

    Repository3

--- src/test/java/kr/ac/kopo/board2024/repository/BoardRepositoryTests.java
+++ src/test/java/kr/ac/kopo/board2024/repository/BoardRepositoryTests.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -47,5 +48,12 @@ public class BoardRepositoryTests {
         Object result=boardRepository.getBoardWithWriter(10L);
         Object[] arr=(Object[]) result;
         System.out.println(Arrays.toString(arr));
+    }
+    @Test
+    public void testReadWithReply(){
+        List<Object[]> result=boardRepository.getBoardWithReply(77L);
+        for (Object[] arr: result){
+            System.out.println(Arrays.toString(arr));
+        }
     }
 }
